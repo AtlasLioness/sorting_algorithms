5465fe2d0f7110a3da1a4a4bb3084c652707e5d7
AtlasLioness <narjisaliane@gmail.com>
2023-09-25 12:57:49 +0000

    0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of ints in asc order w/ bubble sort
+ * @array: array to sort
+ * @size: size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, n = 0;
+
+	if (array == NULL)
+		return;
+
+	while (n < size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		n++;
+	}
+}
