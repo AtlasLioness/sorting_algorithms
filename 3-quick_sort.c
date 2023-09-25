0bdc2c1828188241ece00d1dc800a6b4bf86342a
AtlasLioness <narjisaliane@gmail.com>
2023-09-25 20:51:24 +0000

    3. Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts array of ints using Quick sort
+ * @array: array to sort
+ * @size: size of int
+ *
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 0)
+		return;
+	lomuto(array, 0, size - 1, size);
+}
+
+/**
+ * lomuto - uses Lomuto partition scheme
+ * @array: array to sort
+ * @lo: lowest index
+ * @hi: highest index
+ * @size: size of array
+ *
+ */
+void lomuto(int *array, int lo, int hi, size_t size)
+{
+	int part;
+
+	if (lo < hi)
+	{
+		part = partition(array, lo, hi, size);
+		lomuto(array, lo, part - 1, size);
+		lomuto(array, part + 1, hi, size);
+	}
+}
+
+/**
+ * partition - Lomuto array partition
+ * @array: array to sort
+ * @lo: lowest index
+ * @hi: highest index
+ * @size: size of array
+ *
+ * Return: index of the pivot
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot, index;
+	int i, j;
+
+	pivot = array[hi];
+	i = lo - 1;
+
+	for (j = lo; j <= hi - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swaping(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	index = i + 1;
+	swaping(&array[index], &array[hi]);
+	print_array(array, size);
+	return (index);
+
+}
+
+/**
+ * swaping - swaps two ints in array
+ * @a: ptr to first int
+ * @b: ptr to second int
+ *
+ */
+void swaping(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swaper(listint_t *node1, listint_t *node2, listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void lomuto(int *array, int lo, int hi, size_t siz);
+int partition(int *array, int lo, int hi, size_t size);
+void swaping(int *a, int *b);
 
 #endif /* SORT_H */
